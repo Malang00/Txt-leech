a7e8eb9183e374bcddd1b780372b34e8a6f2275f
Malang00 <91902331+Malang00@users.noreply.github.com>
2024-03-03 21:01:41 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28929701"
-api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
-bot_token = "6893027676:AAEytAdJL-5uY_2LqeCVWA7ziogwPvxBZQw"
+api_id = "8006372"
+api_hash = "f878ef2fd1044167b7d8ab23320e1eda"
+bot_token = "6480499493:AAHUwPdEDV4iR6ED5jnJlqE6EyGyNtwO75M"
